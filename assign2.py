44816f4b8cb1d651c2af8c09b6a3ebedbaa86da2
satyanarayana1998 <97432018+satyanarayana1998@users.noreply.github.com>
2022-06-09 19:42:14 +0530

    Add files via upload

---
+++ assign2.py
@@ -0,0 +1,12 @@
+#Write a program for a Single line comment and multi-line comments
+#this is single line comment
+print("Hello,World!")
+
+#multi_line comment
+comment = """
+This is a comment
+written in
+more than just one line
+"""
+print("Hello,World!")
+print(comment)
